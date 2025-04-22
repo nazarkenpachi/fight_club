9f46d819ccad583e825b0e5a54ff4bb3c3f9b485
NazarKenpachi <nazar.drozd.ip32@gmail.com>
2025-04-22 03:44:02 +0300

    fix deploy issues

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/fight_club/',
   plugins: [react()],
   assetsInclude: ['**/*.PNG', '**/*.MP4'],
 })
